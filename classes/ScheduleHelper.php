9c42ccd029e2ee4c72b675e9b55f29f4b686189e
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2024-01-13 11:13:08 -0500

    Fix calendar overrides

--- classes/ScheduleHelper.php
+++ classes/ScheduleHelper.php
@@ -18,12 +18,17 @@ class ScheduleHelper {
         $overrides = $this->raceDao->selectForDate($date);
         $normal = $this->recurringDao->selectForDate(date('D', strtotime($date)));
 
+        // Remove any conflicts between normal & overrides
         for ($i=0; $i < count($normal) && $overrides !== false; $i++) { 
             $matching_index = array_search($normal[$i]['training_sport'], array_column($overrides, 'race_sport'));
             if ($matching_index !== false) {
                 unset($normal[$i]);
-                array_push($normal, $overrides[$matching_index]);
             }
+        }
+
+        // Add all the overrides
+        for ($i=0; $i < count($overrides); $i++) {
+            array_push($normal, $overrides[$i]);
         }
 
         return $normal;
